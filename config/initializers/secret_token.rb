d9e6d0d65683ddecbcb76ab8b1ea5436f62e0cfe
Ryan Gibbons <rtgibbons23@gmail.com>
2013-03-12 16:30:56 -0600

    empty install of rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyNodejsTest::Application.config.secret_token = 'eb4bf01728235438e9efb745611a52b334061c065b2aeac72aca0aa77897fe2e824060da00d8732e49b77a75ad6dd1651e5cac7567d3efa8f4033f8db128bf2f'
