5b76d98e533ca20cf352920b0b6a8499893f3f93
Ryan Gibbons <rtgibbons23@gmail.com>
2013-03-14 08:47:24 -0600

    grunt task to upload to s3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,34 @@
 module.exports = function(grunt) {
+  var TSTAMP = grunt.template.today('yyyymmddHHMMssl');
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    s3: {
+      options: {
+        key: process.env.AWS_ACCESS_KEY_ID,
+        secret: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: process.env.FOG_DIRECTORY,
+        access: 'public-read'
+      },
+      dev: {
+        // These options override the defaults
+        options: {
+          encodePaths: true,
+          maxOperations: 20
+        },
+        // Files to be uploaded.
+        upload: [
+          {
+            src: 'app/assets/**/*',
+            dest: TSTAMP + '/assets/',
+            gzip: true
+          }
+        ]
+      }
+    }
   });
 
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
+  grunt.registerTask('default', ['s3:dev']);
 
 };
